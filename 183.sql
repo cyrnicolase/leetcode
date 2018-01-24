e0b08deebd229a9c7712ff6a132dda2da1a640a8
cyrnicolase <nicolase.chen@gmail.com>
2018-01-24 13:35:41 +0800

    Update 183.sql

--- 183.sql
+++ 183.sql
@@ -5,3 +5,10 @@
 SELECT Name as Customers FROM Customers 
 WHERE Id NOT IN 
     (SELECT CustomerId FROM Orders); -- 117ms
+
+
+-- 使用Exists 确实效率要高一些
+
+SELECT Name as Customers FROM Customers WHERE NOT EXISTS 
+    (SELECT 1 FROM Orders WHERE CustomerId = Customers.Id);
+
